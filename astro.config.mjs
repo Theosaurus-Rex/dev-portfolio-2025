5542bf7b331734a0c1b2c1d66811084303025282
Theosaurus-Rex <theosaurus13@gmail.com>
2025-11-26 20:21:13 +1100

    chore: Configure build settings and performance/seo optimisations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://theo-harris-dev.com',
   image: {
     domains: ['images.unsplash.com'], // Allow external blog post images
+  },
+  integrations: [sitemap()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  prefetch: {
+    defaultStrategy: 'hover',
+    prefetchAll: true,
   },
 });

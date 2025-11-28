ef00e3f5385467fbc735560c1ddf62da447da68e
Theosaurus-Rex <theosaurus13@gmail.com>
2025-11-28 16:27:51 +1100

    improve: Use Astro Image component for rendering image content in blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ export default defineConfig({
   image: {
     domains: ['images.unsplash.com'], // Allow external blog post images
   },
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+    },
+  },
   integrations: [sitemap()],
   compressHTML: true,
   build: {
